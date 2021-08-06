86b41a24ddbdff7a6b492998c69f31afba0c6ce3
nazar <pesvol34@gmail.com>
2021-08-06 17:44:07 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+const numberOfFilms = prompt("Сколько фильмов посмотрели?", "");
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+
+
+
+console.log(personalMovieDB);
