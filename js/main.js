c624b3a89a5d4f29c865811f33ea5729adf2b8ce
nazar <pesvol34@gmail.com>
2021-08-06 19:51:57 +0300

    second commit

--- js/main.js
+++ js/main.js
@@ -1,11 +1,21 @@
 const numberOfFilms = prompt("Сколько фильмов посмотрели?", "");
-let personalMovieDB = {
+const personalMovieDB = {
     count: numberOfFilms,
     movies: {},
     actors: {},
     genres: [],
     privat: false
 };
+
+const a = prompt("Один из поледних просмотренный фильмов", "");
+const b = prompt("На сколько оцените его?", "");
+const c = prompt("Один из поледних просмотренный фильмов", "");
+const d = prompt("На сколько оцените его?", "");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d
+
+
 
 
 
